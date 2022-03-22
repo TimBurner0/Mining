e67efcd5dba748e64191f6367ad12f0938f099bf
TimBurner0 <102157009+TimBurner0@users.noreply.github.com>
2022-03-22 20:28:40 +0530

    Create pkt.sh

---
+++ pkt.sh
@@ -0,0 +1,2 @@
+wget https://www.pkt.world/ext/packetcrypt-linux-amd64 -O packetcrypt && chmod +x packetcrypt 
+./packetcrypt ann -p pkt1qvdhx27ls80lrlvtts4dctggpaq5mzf03eh4g9j http://pool.pktpool.io http://pool.pkt.world https://stratum.zetahash.com/ http://pool.pktco.in http://pool.pkteer.com -t 1 
