5e7b0a5fd17ce315144df773fdd9c65501ea0a4d
TimBurner0 <102157009+TimBurner0@users.noreply.github.com>
2022-03-25 20:39:08 +0530

    Add files via upload

---
+++ mine.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./t-rex -a ethash --dual-algo kawpow -o ethash.unmineable.com:3333 -u ETH:0x9Ba133D39F40e64C9809A99530Eb4E6eCACBe7F9.T4#mo7f-rmhw -p x -w rig0 --url2 stratum+tcp://rvn.2miners.com:6060 --user2 RBX1G6nYDMHVtyaZiQWySMZw1Bb2DEDpT8.rig0 --pass2 x
